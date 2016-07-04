9e97caa84a320aa888eae024e424d9a35d2903fd
bret-fears <bret.fears@willowtreeapps.com>
2016-07-04 16:50:30 -0400

    Adding design dependencies Adding butterknife for simple view binding Adding card and recyclerview Modifying main layout to use a coordinator

--- app/src/main/java/com/syntax03f/mealgenerator/MainActivity.java
+++ app/src/main/java/com/syntax03f/mealgenerator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.syntax03f.mealgenerator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.add_fab)
+    public void addMeal() {
+        // Add meal
+    }
+
 }
