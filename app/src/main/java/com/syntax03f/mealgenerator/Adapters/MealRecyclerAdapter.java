f74ab7fb460bc4731a7bc012f7f6db226f71cbfe
bret-fears <bret.fears@willowtreeapps.com>
2016-07-04 17:47:55 -0400

    Adding VCS attributes for Android Studio Repositioning FAB to lower right of the screen Adding recycler view adapter scaffolding

---
+++ app/src/main/java/com/syntax03f/mealgenerator/Adapters/MealRecyclerAdapter.java
@@ -0,0 +1,30 @@
+package com.syntax03f.mealgenerator.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by bretfears on 7/4/16.
+ */
+public class MealRecyclerAdapter extends RecyclerView.Adapter {
+
+    public MealRecyclerAdapter() {
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
--- app/src/main/java/com/syntax03f/mealgenerator/MainActivity.java
+++ app/src/main/java/com/syntax03f/mealgenerator/MainActivity.java
@@ -2,6 +2,10 @@ package com.syntax03f.mealgenerator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.syntax03f.mealgenerator.adapters.MealRecyclerAdapter;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -13,11 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        initMealList();
     }
 
     @OnClick(R.id.add_fab)
     public void addMeal() {
         // Add meal
+    }
+
+    public void initMealList() {
+        RecyclerView mealList = (RecyclerView) findViewById(R.id.meal_list);
+        if (mealList == null) return;
+
+        mealList.setLayoutManager(new LinearLayoutManager(this));
+        MealRecyclerAdapter adapter = new MealRecyclerAdapter();
+        mealList.setAdapter(adapter);
     }
 
 }
